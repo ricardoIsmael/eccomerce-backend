96fb5f2da87f9ce1e7b3dcb7cf748eca6fd6595e
ricardoIsmael <ricardo.i.palomino@gmail.com>
2026-04-28 20:49:42 -0500

    a

--- inventory-service/src/main/java/com/ecommerce/inventory_service/service/InventoryServiceImpl.java
+++ inventory-service/src/main/java/com/ecommerce/inventory_service/service/InventoryServiceImpl.java
@@ -8,6 +8,8 @@ import com.ecommerce.inventory_service.model.Inventory;
 import com.ecommerce.inventory_service.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,14 +17,22 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 @Slf4j
+@RefreshScope
 public class InventoryServiceImpl implements  InventoryService
 {
     private final InventoryRepository inventoryRepository;
     private final InventoryMapper inventoryMapper;
 
+    @Value("${inventory.allow-backorders:false}")
+    private boolean allowBackorders;
+
     @Override
     @Transactional(readOnly = true) //conjunto de operaciones roolback
     public boolean isInStock(String sku, Integer quantity) {
+        if(allowBackorders) {
+            log.warn("MODO BACKORDER ACTIVO: Autorizando stock para sku: {}", sku);
+            return true;
+        }
         return inventoryRepository.findBySku(sku)
                 .map( i -> i.getQuantity()>= quantity)
                 .orElse(false); //sino
