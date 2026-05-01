fd47ac988a39d9ef0822aaf1db303c1a4d3c0dbc
ricardoIsmael <ricardo.i.palomino@gmail.com>
2026-05-01 14:06:46 -0500

    a

--- api-gateway/src/main/java/com/ecommerce/api_gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/ecommerce/api_gateway/config/SecurityConfig.java
@@ -30,7 +30,18 @@ public class SecurityConfig
                         .pathMatchers(HttpMethod.GET,"/api/v1/inventory/**").permitAll()
                         .pathMatchers("/api/v1/product/**").hasRole(Role.ADMIN.name())
                         .pathMatchers("/api/v1/inventory/**").hasRole(Role.ADMIN.name())
-                        .pathMatchers(HttpMethod.POST,"/api/v1/oder/**").hasRole(Role.USER.name())
+
+
+                        .pathMatchers(HttpMethod.POST,"/api/v1/order").hasRole(Role.USER.name())
+
+                        .pathMatchers(HttpMethod.GET,"/api/v1/order/**").hasAnyRole(Role.ADMIN.name(),Role.USER.name())
+                        .pathMatchers(HttpMethod.DELETE,"/api/v1/order/**").hasRole(Role.ADMIN.name())
+                        .pathMatchers(HttpMethod.PUT,"/api/v1/oder/**").hasRole(Role.USER.name())
+
+
+
+
+
                         .pathMatchers("/api/v1/order/**").hasRole(Role.ADMIN.name())
                                 .anyExchange().authenticated()
                 )
