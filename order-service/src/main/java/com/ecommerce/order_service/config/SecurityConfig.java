f0278380af504803a362adb905fe7cbbec658224
ricardoIsmael <ricardo.i.palomino@gmail.com>
2026-05-02 16:20:16 -0500

    a

--- order-service/src/main/java/com/ecommerce/order_service/config/SecurityConfig.java
+++ order-service/src/main/java/com/ecommerce/order_service/config/SecurityConfig.java
@@ -1,4 +1,27 @@
 package com.ecommerce.order_service.config;
 
-public class SecurityConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig
+{
+    @Bean
+    public SecurityFilterChain securityWebFilterChain(HttpSecurity httpSecurity){
+        httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests( auth -> auth
+                        .requestMatchers("/actuator/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(
+                        oauth2 ->
+                                oauth2.jwt(Customizer.withDefaults())
+                );
+        return httpSecurity.build();
+    }
 }
